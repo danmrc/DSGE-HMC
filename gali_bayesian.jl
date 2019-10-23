4d664b1261805ab271bd0f64de16dab30b8af058
gilbertoboaretto <56089310+gilbertoboaretto@users.noreply.github.com>
2019-10-22 21:32:19 -0300

    Update gali_bayesian.jl

--- gali_bayesian.jl
+++ gali_bayesian.jl
@@ -1,19 +1,19 @@
-GAMMA_0 = [bet    0      0 0;
-           1       -sig 0 0;
-           0 0 1 -1;
-           0       0      0 1]
+GAMMA_0 = [bet    0     0  0;
+           1      sig  -1  0;
+           0      0     1  0;
+           0      0     0  1]
 
-GAMMA_1 = [1   -kappa 0 0;
-           0    sig  1 0;
-          -phi_pi -phi_y    1 -1;
-           0 0      0 rho_v]
+GAMMA_1 = [1      -kappa  0  0;
+           0       sig    0  0;
+           phi_pi  phi_y  0  1;
+           0       0      0  rho_v]
 
 PSI = [0; 0; 0; 1]
 
-PI = [bet 0;
-      1     -sig;
-      0     0;
-      0     0]
+PI = [bet  0;
+      1   -sig;
+      0    0;
+      0    0]
 
 G0 = GAMMA_0
 G1 = GAMMA_1
